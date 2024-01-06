fb3eb4a20063765b69bf64758b8d4586cb80b519
Hòa Đặng <dangnhathoa22@gmail.com>
2024-01-06 21:24:26 +0700

    update user

--- src/Models/Users.js
+++ src/Models/Users.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema(
     {
-        name: {type: String, require: true},
-        date: {type: String, require: true},
-        email: {type: String, require:true, unique: true},
-        phone: {type: String, require: true},
-        password: {type: String, require: true},
+        name: { type: String, require: true },
+        date: { type: String, require: true },
+        email: { type: String, require: true, unique: true },
+        phone: { type: String, require: true },
+        password: { type: String, require: true },
 
-        access_token: {type: String, require: true},
-        refresh_token: {type: String, require: true},
     },
     {
         timestamps: true,
