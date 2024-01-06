349d9de71e43ff25cc2bb323c96d9ac7f3e8ae84
Nghĩa <98103049+NghiaLe128@users.noreply.github.com>
2024-01-06 20:58:55 +0700

    update user, panel

---
+++ src/Models/Users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema(
+    {
+        name: {type: String, require: true},
+        date: {type: String, require: true},
+        email: {type: String, require:true, unique: true},
+        phone: {type: String, require: true},
+        password: {type: String, require: true},
+
+        access_token: {type: String, require: true},
+        refresh_token: {type: String, require: true},
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model("Users", userSchema);
+module.exports = User;
