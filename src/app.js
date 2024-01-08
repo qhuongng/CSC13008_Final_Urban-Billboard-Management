ed4a2ef6152bdfa0c857f797b30719301a7ca881
qhuongng <nq.huong.2304@gmail.com>
2024-01-08 13:11:56 +0700

    Add ward & district info for free point reports; add reCAPTCHA verificcation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const routes = require("./Routers");
 const bodyParser = require("body-parser");
 const path = require("path");
 const handlebars = require("express-handlebars");
+const cors = require("cors");
 //---------
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+app.use(cors());
 
 routes(app);
 
@@ -28,6 +30,35 @@ app.set("views", path.join(__dirname, "Views"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post('/verify-recaptcha', async (req, res) => {
+  const recaptchaVerifyUrl = 'https://www.google.com/recaptcha/api/siteverify';
+  const recaptchaSecretKey = '6LewcUkpAAAAAPO-7HOQAFSEvdKHC_gOfsvXh9oD';
+
+  try {
+    const recaptchaVerificationResponse = await fetch(recaptchaVerifyUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        secret: recaptchaSecretKey,
+        response: req.body.token,
+      }),
+    });
+
+    const recaptchaVerificationData = await recaptchaVerificationResponse.json();
+
+    if (!recaptchaVerificationData.success) {
+      res.status(403).json({ success: false });
+    } else {
+      res.status(200).json({ success: true });
+    }
+  } catch (error) {
+    console.error('Error verifying reCAPTCHA:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
 });
 
 app.listen(process.env.PORT, () => {
