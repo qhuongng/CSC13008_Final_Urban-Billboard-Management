8117b540b800d7e4b981693e70f902b193108135
Nghĩa <98103049+NghiaLe128@users.noreply.github.com>
2024-01-09 16:23:46 +0700

    update access token

--- src/Services/JwtService.js
+++ src/Services/JwtService.js
@@ -5,21 +5,12 @@ dotenv.config()
 const generalAccessToken = async (payload) => {
     const access_token = jwt.sign({
         payload
-    }, process.env.ACCESS_TOKEN, { expiresIn: '30d' })
+    }, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
 
     return access_token
 }
 
-const generalRefreshToken = async (payload) => {
-    const refresh_token = jwt.sign({
-        payload
-    }, process.env.REFRESH_TOKEN, { expiresIn: '365d' })
-
-    return refresh_token
-}
-
 
 module.exports = {
-    generalAccessToken,
-    generalRefreshToken
+    generalAccessToken
 }
