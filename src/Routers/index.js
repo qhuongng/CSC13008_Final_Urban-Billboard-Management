21c8bdb6a09ddf1f48af97149b7c4daffa94eb07
Hòa Đặng <dangnhathoa22@gmail.com>
2024-01-11 22:17:39 +0700

    update navbart

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -28,27 +28,7 @@ const routes = (app) => {
   app.use('/api/otp', OtpRouter);
   app.use('/api/controlWardDistrict', controlWardDistrictRouter);
   //
-
-
   app.get("/", (req, res) => {
-    const navItemDepartment = [
-      { link: "/api/controlWardDistrict", label: "Quản lí Phường, Quận" },
-      { link: "#", label: "Quản lí loại hình quảng cáo" },
-      { link: "#", label: "Quản lí hình thức báo cáo" },
-      { link: "#", label: "Quản lí điểm đặt quảng cáo" },
-      { link: "#", label: "Quản lí bảng quảng cáo" },
-      { link: "#", label: "Xét duyệt yêu cầu chỉnh sửa" },
-      { link: "#", label: "Xét duyệt yêu cầu cấp phép" },
-      { link: "#", label: "Thống kê báo cáo" },
-      { link: "#", label: "Tạo tài khoản cán bộ" },
-      { link: "#", label: "Phân công khu vực" },
-    ];
-    const navItemAnother = [
-      { link: "#", label: "Thống kê điểm đặt, biển quảng cáo" },
-      { link: "#", label: "Gửi yêu cầu chỉnh sửa" },
-      { link: "#", label: "Xử lí báo cáo" },
-      { link: "#", label: "Tạo yêu cầu cấp phép quảng cáo" },
-    ];
     if (res.locals.auth == false) {
       res.render("viewUser/login", {
         layout: false
