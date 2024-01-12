9624b2852b48c2028e53ba1a34b971ee55f3112a
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-01-12 12:04:24 +0700

    update editing the report type to db

--- src/Controllers/reportType.controllers.js
+++ src/Controllers/reportType.controllers.js
@@ -1,35 +1,50 @@
-const ReportTypeService = require('../Services/reportType.services')
+const ReportTypeService = require("../Services/reportType.services");
 
-const createTypeReport= async(req,res)=>{
-    try{
-        const {reportId, reportName} = req.body
+const createTypeReport = async (req, res) => {
+  try {
+    const { reportId, reportName } = req.body;
 
-        if(!reportId || !reportName ){
-            return res.status(404).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        }
-        const response = await ReportTypeService.createTypeReport(req.body)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
+    if (!reportId || !reportName) {
+      return res.status(404).json({
+        status: "ERR",
+        message: "The input is required",
+      });
     }
-}
+    const response = await ReportTypeService.createTypeReport(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getAllReportType = async (req, res) => {
-    try {
-        const response = await ReportTypeService.getAllReportType()
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    const response = await ReportTypeService.getAllReportType();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
+const updateReportType = async (req, res) => {
+  try {
+    const { id, editedData } = req.body;
+    console.log(id, editedData);
+    return res.status(200).json({
+      message: "OK",
+    });
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 module.exports = {
-    createTypeReport,
-    getAllReportType
-}
+  createTypeReport,
+  getAllReportType,
+  updateReportType,
+};
