a183d339fa6252de60d36323ea62c54f1e56a0db
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-01-12 12:09:30 +0700

    update editting the panel type to db

--- src/Controllers/panelType.controllers.js
+++ src/Controllers/panelType.controllers.js
@@ -1,34 +1,49 @@
-const PanelService = require('../Services/panelType.services')
+const PanelService = require("../Services/panelType.services");
 
-const createTypePan= async(req,res)=>{
-    try{
-        const {panId, panName} = req.body
+const createTypePan = async (req, res) => {
+  try {
+    const { panId, panName } = req.body;
 
-        if(!panId || !panName ){
-            return res.status(404).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        }
-        const response = await PanelService.createTypePan(req.body)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
+    if (!panId || !panName) {
+      return res.status(404).json({
+        status: "ERR",
+        message: "The input is required",
+      });
     }
-}
+    const response = await PanelService.createTypePan(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 const getAllPanelType = async (req, res) => {
-    try {
-        const response = await PanelService.getAllPanelType()
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    const response = await PanelService.getAllPanelType();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
+const updatePanelType = async (req, res) => {
+  try {
+    const { id, editedData } = req.body;
+    console.log(id, editedData);
+    return res.status(200).json({
+      message: "OK",
+    });
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 module.exports = {
-    createTypePan,
-    getAllPanelType
-}
+  createTypePan,
+  getAllPanelType,
+  updatePanelType,
+};
