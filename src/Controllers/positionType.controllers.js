75f3da5b5913d3d6c0451a0cc8ac42f1e55d7a1b
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-01-14 16:23:32 +0700

    update getPositionType in add new Point, unfinish edit and delete

--- src/Controllers/positionType.controllers.js
+++ src/Controllers/positionType.controllers.js
@@ -1,23 +1,35 @@
-const PositionService = require('../Services/positionType.services')
+const PositionService = require("../Services/positionType.services");
 
-const createTypePos= async(req,res)=>{
-    try{
-        const {posId, posName} = req.body
+const createTypePos = async (req, res) => {
+  try {
+    const { posId, posName } = req.body;
 
-        if(!posId || !posName ){
-            return res.status(404).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        }
-        const response = await PositionService.createTypePos(req.body)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
+    if (!posId || !posName) {
+      return res.status(404).json({
+        status: "ERR",
+        message: "The input is required",
+      });
     }
-}
+    const response = await PositionService.createTypePos(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
+const getAllTypePos = async (req, res) => {
+  try {
+    const response = await PositionService.getAllTypePos();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 module.exports = {
-    createTypePos
-}
+  createTypePos,
+  getAllTypePos,
+};
--- src/Routers/positionType.routes.js
+++ src/Routers/positionType.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router()
 const posController = require('../Controllers/positionType.controllers');
 
-router.post('/createTypePos', posController.createTypePos),
+router.post('/createTypePos', posController.createTypePos);
+router.get('/getAllTypePos', posController.getAllTypePos);
 
 module.exports = router
