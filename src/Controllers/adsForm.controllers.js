dda4afc8e7c176e1a5e98e0871755555a828926c
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-01-14 15:51:15 +0700

    update adsForm to handle all form

--- src/Controllers/adsForm.controllers.js
+++ src/Controllers/adsForm.controllers.js
@@ -1,25 +1,37 @@
-const adsFormService = require('../Services/adsForm.services')
+const adsFormService = require("../Services/adsForm.services");
 
-const createForm= async(req,res)=>{
-    try{
-        const {formId, formName} = req.body
+const createForm = async (req, res) => {
+  try {
+    const { formId, formName } = req.body;
 
-        if(!formId || !formName ){
-            return res.status(404).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        }
-        const response = await adsFormService.createForm(req.body)
-        //sau khi lấy xong sẽ không return về data nữa mà chỉnh data cho phù hợp với FE rồi render và gắn data cho FE
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
+    if (!formId || !formName) {
+      return res.status(404).json({
+        status: "ERR",
+        message: "The input is required",
+      });
     }
-}
+    const response = await adsFormService.createForm(req.body);
+    //sau khi lấy xong sẽ không return về data nữa mà chỉnh data cho phù hợp với FE rồi render và gắn data cho FE
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
+const getAllForm = async (req, res) => {
+  try {
+    const response = await adsFormService.getAllForm();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 module.exports = {
-    createForm
-}
+  createForm,
+  getAllForm,
+};
