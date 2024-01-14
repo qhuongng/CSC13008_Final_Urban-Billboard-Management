cf542c0b1d26100a13854b1e3e97c4583c078141
Hòa Đặng <dangnhathoa22@gmail.com>
2024-01-14 21:24:58 +0700

    update license

--- src/Controllers/license.controllers.js
+++ src/Controllers/license.controllers.js
@@ -36,7 +36,7 @@ const getAllLicense = async (req, res) => {
 
 const getAcceptedLicenseByIdPanel = async (req, res) => {
     try {
-        const idPanel = req.body.idPanel
+        const idPanel = req.params.idPanel
         const listLicense = await licenseServices.getAcceptedLicenseByIdPanel(idPanel);
         res.status(200).json(listLicense);
     } catch (error) {
