83974fda522c182ac9d8cc17fb125df494095490
qhuongng <nq.huong.2304@gmail.com>
2024-01-14 14:46:25 +0700

    Add license request form

--- src/Routers/license.routes.js
+++ src/Routers/license.routes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const licenseController = require('../Controllers/license.controllers')
 const multer = require('multer');
 
+router.get('/', (req, res) => {
+    res.render('viewLicense/licenseForm');
+});
+
 router.post('/createLicense', licenseController.createLicense);
 router.get('/getAllLicense', licenseController.getAllLicense);
 router.get('/getLicenseByIdPanel', licenseController.getAcceptedLicenseByIdPanel);
