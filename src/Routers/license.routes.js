3517cf29ead7a41f4bd11a2be670b02639bb8fcf
Hòa Đặng <dangnhathoa22@gmail.com>
2024-01-14 10:50:08 +0700

    update send mail when action handler changed

--- src/Routers/license.routes.js
+++ src/Routers/license.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router()
 const licenseController = require('../Controllers/license.controllers')
 const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-router.post('/createLicense', licenseController.createLicense);
+router.post('/createLicense', upload.single('image'), licenseController.createLicense);
 router.get('/getAllLicense', licenseController.getAllLicense);
 router.get('/getLicenseByIdPanel', licenseController.getAcceptedLicenseByIdPanel);
 router.put('/udpateAccept', licenseController.updateAccept);
