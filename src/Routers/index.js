7a66498d212070b9edf1b1acf2bb37fd6db581e9
Nghĩa <98103049+NghiaLe128@users.noreply.github.com>
2024-01-15 12:03:25 +0700

    update create controlLicense

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -23,6 +23,7 @@ const controlReportRouter = require('./controlReport.routes');
 const licenseRouter = require('./license.routes');
 const ProfileRouter = require('./profile.routes')
 const licenseImgRouter = require('./licenseImg.routes');
+const controlLicense = require('./controlLicense.routes')
 
 const routes = (app) => {
   // đường dẫn dùng cho citizen
@@ -50,6 +51,7 @@ const routes = (app) => {
   app.use('/api/license', licenseRouter);
   app.use('/api/profile', ProfileRouter)
   app.use('/api/licenseImg', licenseImgRouter);
+  app.use('/api/controlLicense', controlLicense)
   //
   app.get("/", (req, res) => {
     if (res.locals.auth == false) {
